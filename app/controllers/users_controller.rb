1d933c4796b6a538c4b6df7b08b3b240de8bcbdf
Matt Herich <mjherich@gmail.com>
2015-09-23 20:17:17 +0000

    Fixed users controller filters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :only_current_user
   
   def show
     @user = User.find( params[:id] ) # :id is used because that is what is used in our routes.rb file
   end
-  
-  private
-    def only_current_user
-      @user = User.find( params[:id] )
-      redirect_to(root_url) unless @user == current_user
-    end
 end
